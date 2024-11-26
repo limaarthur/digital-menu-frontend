de40f90cf0fd761eb16a0e66ba59b153a037d035
Arthur Lima <artglima.dev@gmail.com>
2024-11-26 14:56:35 -0300

    feat: installing and configuring Tanstack and axios

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,22 @@
+import { Card } from "./components/Card";
+import { useFoodData } from "./hooks/useFoodData";
+import './app.css'
+
 export function App() {
+  const { data } = useFoodData();
+
   return (
     <div className="container">
       <h1>Cardápio</h1>
+        <div className="card-grid">
+          {data?.map(foodData => 
+            <Card 
+              price={foodData.price} 
+              title={foodData.title} 
+              imgUrl={foodData.imgUrl}
+              />
+          )}
+      </div>
     </div>
-   
   )
 }
